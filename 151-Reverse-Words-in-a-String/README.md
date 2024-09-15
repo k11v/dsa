5688b725ac111ae3144a5472f342e1a2bcd9f5e7
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-09-15 21:41:13 +0300

    Add 151-Reverse-Words-in-a-String with O(1) memory

---
+++ 151-Reverse-Words-in-a-String/README.md
@@ -0,0 +1,8 @@
+---
+- get problem: 1m 36s
+- get solution with library: 2m 28s
+- code with library (accepted): 4m 25s
+- get solution (O(n²) speed, O(1) memory): 44m 53s
+- get solution (O(n) speed, O(1) memory, with great help): 12m 55s
+- code with O(1) memory (accepted): 45m 34s
+---
--- 151-Reverse-Words-in-a-String/main_test.go
+++ 151-Reverse-Words-in-a-String/main_test.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"slices"
 	"strconv"
-	"strings"
 	"testing"
 )
 
@@ -28,8 +26,47 @@ func TestReverseWords(t *testing.T) {
 
 // LeetCode
 
+func reverse(rs []rune, l, r int) {
+	for i := 0; i < (r - l + 1) / 2; i++ {
+		rs[l + i], rs[r - i] = rs[r - i], rs[l + i]
+	}
+}
+
 func reverseWords(s string) string {
-	words := strings.Fields(s)
-	slices.Reverse(words)
-	return strings.Join(words, " ")
+	rs := []rune(s)
+
+	reverse(rs, 0, len(rs) - 1)
+	wordStart := -1
+	for i := 0; i < len(rs); i++ {
+		if rs[i] == ' ' {
+			if wordStart != -1 {
+				reverse(rs, wordStart, i - 1)
+				wordStart = -1
+			}
+		} else {
+			if wordStart == -1 {
+				wordStart = i
+			}
+		}
+	}
+	if wordStart != -1 {
+		reverse(rs, wordStart, len(rs) - 1)
+		wordStart = -1
+	}
+
+	w := 0
+	for r := 0; r < len(rs); r++ {
+		if rs[r] != ' ' {
+			rs[w] = rs[r]
+			w++
+		} else {
+			if w != 0 && r != len(rs) - 1 && rs[r + 1] != ' ' {
+				rs[w] = ' '
+				w++
+			}
+		}
+	}
+	rs = rs[0:w]
+
+	return string(rs)
 }
