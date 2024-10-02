14e9ee0d51ae86194be5f4e9a695c20d0ce1c79e
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-10-02 20:48:00 +0300

    Add 236-Lowest-Common-Ancestor-of-a-Binary-Tree without path

--- 236-Lowest-Common-Ancestor-of-a-Binary-Tree/README.md
+++ 236-Lowest-Common-Ancestor-of-a-Binary-Tree/README.md
@@ -1,6 +1,13 @@
 ---
-- get problem: 0s
-- get solution: 0s
-- code: 0s
-- code to fix: 0s
+- get problem: 1m 39s
+- get solution: 6m 58s
+- code: 11m 1s
+- check and fix: 1m 34s
+- submit → RE: 15s
+- fix: 2m 22s
+- submit → RE: 15s
+- fix: 4m 50s
+- submit → OK: 30s
+- code without path with help: 10m 25s
+- submit → OK: 15s
 ---
--- 236-Lowest-Common-Ancestor-of-a-Binary-Tree/main_test.go
+++ 236-Lowest-Common-Ancestor-of-a-Binary-Tree/main_test.go
@@ -11,40 +11,21 @@ type TreeNode struct {
 // LeetCode
 
 func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
-	pathToP, err := path(root, p)
-	if err != nil {
-		panic(err)
-	}
-
-	pathToQ, err := path(root, q)
-	if err != nil {
-		panic(err)
-	}
-	
-	i := 0
-	for i+1 < len(pathToP) && i+1 < len(pathToQ) && pathToP[i+1] == pathToQ[i+1] {
-		i++
-	}
-
-	return pathToP[i]
-}
-
-func path(root, n *TreeNode) ([]*TreeNode, error) {
 	if root == nil {
-		return nil, fmt.Errorf("root is nil")
+		return nil
+	}
+	if root == p || root == q {
+		return root
 	}
 
-	p := []*TreeNode{root}
-	if root.Val == n.Val {
-		return p, nil
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	if left != nil && right != nil {
+		return root
 	}
-	if subpath, err := path(root.Left, n); err != nil {
-		p = append(p, subpath...)
-		return p, nil
+	if left != nil {
+		return left
 	}
-	if subpath, err := path(root.Right, n); err != nil {
-		p = append(p, subpath...)
-		return p, nil
-	}
-	return nil, fmt.Errorf("node %d is not a descendant of root %d", root.Val, n.Val)
+	return right
 }
