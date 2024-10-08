ecefa52112c1943410db4d623630265f06fd2da5
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-10-08 15:34:06 +0300

    Add 380-Insert-Delete-GetRandom-O-1 with map and slice

--- 380-Insert-Delete-GetRandom-O-1/main_test.go
+++ 380-Insert-Delete-GetRandom-O-1/main_test.go
@@ -2,83 +2,49 @@ package main
 
 import (
 	"math/rand/v2"
-	"hash/maphash"
-	"encoding/binary"
 )
 
 // LeetCode
 
 type RandomizedSet struct {
-	buckets [][]int
-	maxBucketSize int
-	length int
-	h *maphash.Hash
+	m map[int]int
+	a []int
 }
 
 func Constructor() RandomizedSet {
-	return RandomizedSet{buckets: make([][]int, 1), maxBucketSize: 8, length: 0, h: &maphash.Hash{}}
+	return RandomizedSet{m: make(map[int]int), a: make([]int, 0)}
 }
 
 func (s *RandomizedSet) Insert(v int) bool {
-	b := s.hash(v) % uint64(len(s.buckets))
-
-	for i := 0; i < len(s.buckets[b]); i++ {
-		if s.buckets[b][i] == v {
-			return false
-		}
+	if _, present := s.m[v]; present {
+		return false
 	}
-
-	if len(s.buckets[b]) == s.maxBucketSize {
-		s.grow()
-		b = s.hash(v) % uint64(len(s.buckets))
-	}
-
-	s.buckets[b] = append(s.buckets[b], v)
-	s.length++
+	s.a = append(s.a, v)
+	s.m[v] = len(s.a)-1
 	return true
 }
 
 func (s *RandomizedSet) Remove(v int) bool {
-	b := s.hash(v) % uint64(len(s.buckets))
-
-	for i := 0; i < len(s.buckets[b]); i++ {
-		if s.buckets[b][i] == v {
-			s.buckets[b] = append(s.buckets[b][:i], s.buckets[b][i+1:]...)
-			s.length--
-			return true
-		}
+	i, present := s.m[v]
+	if !present {
+		return false
 	}
 
-	return false
+	end := len(s.a)-1
+	endValue := s.a[end]
+
+	s.a[i] = s.a[end]
+	s.m[endValue] = i
+	s.a = s.a[:len(s.a)-1]
+	delete(s.m, v)
+
+	return true
 }
 
 func (s *RandomizedSet) GetRandom() int {
-	if s.length == 0 {
+	if len(s.m) == 0 {
 		panic("randomized set is empty")
 	}
-
-	for {
-		b := rand.IntN(len(s.buckets))
-		if len(s.buckets[b]) != 0 {
-			i := rand.IntN(len(s.buckets[b]))
-			return s.buckets[b][i]
-		}
-	}
-}
-
-func (s *RandomizedSet) grow() {
-	newBuckets := make([][]int, len(s.buckets)*2)
-	for b := 0; b < len(s.buckets); b++ {
-		for i := 0; i < len(s.buckets[b]); i++ {
-			newB := s.hash(s.buckets[b][i]) % uint64(len(newBuckets))
-			newBuckets[newB] = append(newBuckets[newB], s.buckets[b][i])
-		}
-	}
-	s.buckets = newBuckets
-}
-
-func (s *RandomizedSet) hash(v int) uint64 {
-	s.h.Reset()
-	binary.Write(s.h, binary.LittleEndian, v)
-	return s.h.Sum64()
+	i := rand.IntN(len(s.a))
+	return s.a[i]
 }
