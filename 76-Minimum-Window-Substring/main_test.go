2bb64b63b88f1ccac47fa3eb3f210584fe0f90d4
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-10-10 00:31:55 +0300

    Add 76-Minimum-Window-Substring with refactor

--- 76-Minimum-Window-Substring/main_test.go
+++ 76-Minimum-Window-Substring/main_test.go
@@ -5,64 +5,43 @@ package main
 func minWindow(s string, t string) string {
 	got := make(map[byte]int)
 	want := make(map[byte]int)
-
 	for i := 0; i < len(t); i++ {
 		want[t[i]]++
 	}
-
-	l := 0
-	for l < len(s) && want[s[l]] == 0 {
-		l++
-	}
-	if l == len(s) {
-		return ""
-	}
-
-	r := l
 	gotComplete := 0
 	wantComplete := len(want)
-	for gotComplete < wantComplete {
-		for r < len(s) && want[s[r]] == 0 {
-			r++
-		}
-		if r == len(s) {
-			return ""
-		}
-		got[s[r]]++
-		if got[s[r]] == want[s[r]] {
-			gotComplete++
-		}
-		r++
-	}
-	r--
 
-	minL, minR := l, r
-outer:
-	for {
-		for got[s[l]] > want[s[l]] {
-			got[s[l]]--
-			l++
-			for l < len(s) && want[s[l]] == 0 {
-				l++
-			}
-			if l == len(s) {
-				break outer
+
+	minB, minE := 0, len(s)
+	b, e := 0, 0
+	for e < len(s) {
+		if want[s[e]] > 0 {
+			got[s[e]]++
+			if got[s[e]] == want[s[e]] {
+				gotComplete++
 			}
 		}
+		e++
 
-		if r-l < minR-minL {
-			minL, minR = l, r
-		}
+		if gotComplete == wantComplete {
+			for b < len(s) {
+				if want[s[b]] > 0 {
+					if got[s[b]] == want[s[b]] {
+						break
+					}
+					got[s[b]]--
+				}
+				b++
+			}
 
-		r++
-		for r < len(s) && want[s[r]] == 0 {
-			r++
+			if e-b < minE-minB {
+				minB, minE = b, e
+			}
 		}
-		if r == len(s) {
-			break outer
-		}
-		got[s[r]]++
 	}
 
-	return s[minL:minR+1]
+	if gotComplete != wantComplete {
+		return ""
+	}
+	return s[minB:minE]
 }
