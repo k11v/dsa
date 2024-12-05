d0d65906de6ab24a3706654fbcb1b469d7162545
Kirill Gashkov <66561526+k11v@users.noreply.github.com>
2024-12-05 23:46:12 +0300

    Tidy

--- adventofcode.com/2024-2-red-nosed-reports/part-2/main.go
+++ adventofcode.com/2024-2-red-nosed-reports/part-2/main.go
@@ -6,7 +6,6 @@ import (
 	"bufio"
 	"strings"
 	"strconv"
-	"flag"
 )
 
 func main() {
@@ -16,19 +15,6 @@ func main() {
 }
 
 func run() error {
-	flag.Parse()
-
-	switch flag.Arg(0) {
-	case "1":
-		return run1()
-	case "2":
-		return run2()
-	default:
-		panic("unknown part")
-	}
-}
-
-func run1() error {
 	safeCount := 0
 	scanner := bufio.NewScanner(os.Stdin)
 	for scanner.Scan() {
@@ -46,44 +32,6 @@ func run1() error {
 }
 
 func isSafe(r []int) bool {
-	if len(r) <= 1 {
-		return true
-	}
-
-	increasing := r[1] - r[0] > 0
-
-	for i := 1; i < len(r); i++ {
-		directionOK := r[i] - r[i-1] > 0 == increasing
-
-		d := abs(r[i] - r[i-1])
-		diffOK := d >= 1 && d <= 3 
-
-		if !directionOK || !diffOK {
-			return false
-		}
-	}
-
-	return true
-}
-
-func run2() error {
-	safeCount := 0
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		nums, err := intFields(scanner.Text())
-		if err != nil {
-			return err
-		}
-
-		if isSafeWithTolerance(nums) {
-			safeCount++
-		}
-	}
-	fmt.Println(safeCount)
-	return nil
-}
-
-func isSafeWithTolerance(r []int) bool {
 	return isSafeWithTolerateAndIncreasing(r, true, false) || isSafeWithTolerateAndIncreasing(r, true, true)
 }
 
@@ -106,43 +54,6 @@ func isSafeWithTolerateAndIncreasing(r []int, tolerate bool, increasing bool) bo
 			}
 			return false
 		}
-	}
-
-	return true
-}
-
-func isSafeWithSkippedIndex(r []int, skippedIndex *int) bool {
-	if len(r) <= 1 {
-		return true
-	}
-
-	increasing := r[1] - r[0] > 0
-
-	var prev int
-	for i, curr := range r {
-		if skippedIndex != nil && i == *skippedIndex {
-			continue
-		}
-
-		if i == 0 || i == 1 && skippedIndex != nil && *skippedIndex == 0 {
-			prev = curr
-			continue
-		}
-
-		directionOK := curr - prev > 0 == increasing
-
-		d := abs(curr - prev)
-		diffOK := d >= 1 && d <= 3 
-
-		if !directionOK || !diffOK {
-			if skippedIndex != nil {
-				return false
-			}
-			prevIndex := i-1
-			currIndex := i
-			return isSafeWithSkippedIndex(r, &prevIndex) || isSafeWithSkippedIndex(r, &currIndex)
-		}
-		prev = curr
 	}
 
 	return true
