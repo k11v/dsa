dc86ee35f767d7739579ccffc02cd63cd39e88d3
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2024-12-11 22:27:21 +0300

    Add adventofcode.com/2024-11-plutonian-pebbles

---
+++ adventofcode.com/2024-11-plutonian-pebbles/README.md
@@ -0,0 +1,11 @@
+Time to solve:
+
+- read: 4m 32s
+- think: 4m 55s
+- code: 15m 7s
+- run and fix: 1m 2s
+- run and fix: 51s
+- success: 15s
+- read: 41s
+- success: 1s
+- _total: 27m 24s_
---
+++ adventofcode.com/2024-11-plutonian-pebbles/part-2/main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"strconv"
+)
+
+func main() {
+	countFromNumAndBlinks := make(map[struct{ num, blinks int }]int)
+
+	var count func(num, blinks int) int
+	count = func(num, blinks int) int {
+		if countFromNumAndBlinks[struct{ num, blinks int }{num, blinks}] != 0 {
+			return countFromNumAndBlinks[struct{ num, blinks int }{num, blinks}]
+		}
+		if blinks == 0 {
+			return 1
+		}
+		var c int
+		if num == 0 {
+			c = count(1, blinks-1)
+		} else if left, right, ok := splitIfEven(num); ok {
+			c = count(left, blinks-1) + count(right, blinks-1)
+		} else {
+			c = count(num*2024, blinks-1)
+		}
+		countFromNumAndBlinks[struct{ num, blinks int }{num, blinks}] = c
+		return c
+	}
+
+	answer := 0
+	for {
+		var num int
+		_, err := fmt.Scanf("%d", &num)
+		if err == io.EOF {
+			break
+		}
+		answer += count(num, 75)
+	}
+	fmt.Println(answer)
+}
+
+func splitIfEven(num int) (left int, right int, ok bool) {
+	numString := strconv.Itoa(num)
+	if len(numString)%2 == 0 {
+		m := len(numString) / 2
+		left, err := strconv.Atoi(numString[:m])
+		if err != nil {
+			panic(err)
+		}
+		right, err := strconv.Atoi(numString[m:])
+		if err != nil {
+			panic(err)
+		}
+		return left, right, true
+	}
+	return 0, 0, false
+}
