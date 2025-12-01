d1a5c16c1b3f554d1640dff2b548b36a8a0f8cd4
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-12-01 16:25:58 +0300

    Add adventofcode.com/2025-1-secret-entrance part-1 (2/3)

--- adventofcode.com/2025-1-secret-entrance/src/bin/part-1/main.rs
+++ adventofcode.com/2025-1-secret-entrance/src/bin/part-1/main.rs
@@ -1,3 +1,17 @@
+use std::io;
+use std::io::Read;
+
 fn main() {
-    println!("Hello, world!");
+    // Read input.
+    let mut input = String::new();
+    io::stdin()
+        .read_to_string(&mut input)
+        .expect("stdin should be readable");
+    let input = input;
+
+    // Parse instructions.
+    let instructions = input.split("\n");
+    for instruction in instructions {
+        println!("{}", instruction)
+    }
 }
