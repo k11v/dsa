36d0a22f7b44f93bdbcc465b0c9d20d4bd9951a3
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-12-10 23:32:17 +0300

    Add adventofcode.com/2025-10-factory part-1 (outline problem)

---
+++ adventofcode.com/2025-10-factory/src/bin/part-1/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    // one machine per line
+    // [...] - single indicator light diagram
+    // indicator lights - initially off
+    // (...) (...) - one or more button wiring schematics
+    // buttons toggle linked indicator lights, 0-based
+    // {...} - joltage requirements
+    // ignored in part-1
+    // fewest total presses
+}
