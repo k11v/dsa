5dea4b294d04e66c153077619ff59d836aa059ec
Kirill Gashkov <30258757+k11v@users.noreply.github.com>
2025-12-17 19:28:07 +0300

    Add 2025-5-cafeteria part-1 (draft solution)

--- adventofcode.com/2025-5-cafeteria/src/bin/part-1/main.rs
+++ adventofcode.com/2025-5-cafeteria/src/bin/part-1/main.rs
@@ -19,18 +19,79 @@
 // while num > range: increment range index
 // return result
 
+use std::io;
+use std::io::BufRead;
+
 fn main() {
     let res = 0;
 
-    let lines = io::stdio().lock().lines();
+    let ranges = vec![];
+    let nums = vec![];
+
+    let lines = io::stdin().lock().lines();
 
     for line in lines {
+        let line = match line {
+            Ok(v) => v,
+            Err(e) => panic!("failed to read line: {}", e),
+        };
+
         if line == "" {
             break;
         }
+
+        let (a, b) = match line.split_once('-') {
+            Some(v) => v,
+            None => panic!("failed to parse range: '-' not found"),
+        };
+
+        let a = match a.parse::<i32>() {
+            Ok(v) => v,
+            Err(e) => panic!("failed to parse left boundary of range: {}", e),
+        };
+
+        let b = match b.parse::<i32>() {
+            Ok(v) => v,
+            Err(e) => panic!("failed to parse right boundary of range: {}", e),
+        };
+
+        ranges.append((a, b));
     }
 
-    for line in lines {}
+    for line in lines {
+        let line = match line {
+            Ok(v) => v,
+            Err(e) => panic!("failed to read line: {}", e),
+        };
+
+        let num = match line.parse::<i32>() {
+            Ok(v) => v,
+            Err(e) => panic!("failed to parse num: {}", e),
+        };
+
+        nums.append(num);
+    }
+
+    ranges.sort();
+    nums.sort();
+
+    let mut range_i = 0;
+    let mut num_i = 0;
+
+    while num_i < nums.len() && range_i < ranges.len() {
+        while num_i < nums.len() && range_i < ranges.len() && nums[num_i] < ranges[range_i].0 {
+            num_i += 1;
+        }
+
+        while num_i < nums.len() && range_i < ranges.len() && nums[num_i] <= ranges[range_i].1 {
+            num_i += 1;
+            res += 1;
+        }
+
+        while num_i < nums.len() && range_i < ranges.len() && nums[num_i] > ranges[range_i].1 {
+            range_i += 1;
+        }
+    }
 
     println!("{}", res);
 }
